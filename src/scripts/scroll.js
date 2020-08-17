20e32913c75cf1c5f95c585292a12be116ea9308
Anna Podoplelova <any4451@yandex.ru>
2020-08-17 13:24:25 +0300

    new commit

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -10,26 +10,5 @@ scroll.addEventListener("click", e => {
 
   
 
-const navItems = document.querySelectorAll("[data-target]");
-  
-    function scrollIntoView(e) {
-      e.preventDefault();
-  
-      const item = e.currentTarget;
-      const target = item.getAttribute("data-target");
-      const targetElem = document.querySelector(`.${target}`);
-      const sidebar = document.querySelector(".sidebar");
-  
-      sidebar.classList.remove("active");
-  
-      targetElem.scrollIntoView({
-        block: "center",
-        behavior: "smooth"
-      });
-    }
-  
-    navItems.forEach(item => {
-      item.addEventListener("click", scrollIntoView);
-    });
 
   
